be2aef8a504b4d01d0e3aaacabcce1c243bb5662
abhijeetkhire@gmail.com <abhijeetkhire@gmail.com>
2019-09-12 22:01:26 +0530

    basic file to start with chrome extension

---
+++ content.js
@@ -0,0 +1,7 @@
+alert("Hello This is your first Chrome Extension .");
+
+// chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+//   const re = new RegExp("bear", "gi");
+//   const matches = document.documentElement.innerHTML.match(re);
+//   sendResponse({ count: matches.length });
+// });
